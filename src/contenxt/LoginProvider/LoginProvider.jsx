33209e3cff5bd891277a3d4c7c5a7490a0b4e3cc
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-03-17 17:13:29 -0300

    feat: Login
    criação dos contexts, estilização dos componentes, validação e utilização do hook form.

--- src/contenxt/LoginProvider/LoginProvider.jsx
+++ src/contenxt/LoginProvider/LoginProvider.jsx
@@ -1,22 +1,70 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
+import {
+  requestSessionUser,
+  requestValidationToken,
+  requestGetUser,
+} from '../../service/api'
 
 export const LoginContext = createContext({})
 
 const LoginProvider = ({ children }) => {
   const [token, setToken] = useState('')
+  const [data, setData] = useState(null)
+  const [login, setLogin] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  async function autoLogin() {
+    const token = localStorage.getItem('@DOGS')
+
+    if (token) {
+      try {
+        setError(null)
+        setLoading(true)
+        const res = await requestValidationToken(token)
+
+        GetUser(token)
+        
+      } catch (error) {
+        console.log('Erro na validação do token:', error)
+        setError(error)
+
+      } finally {
+        setLoading(false)
+      }
+    }
+  }
+
+  useEffect(() => {
+    autoLogin()
+  }, [])
+
+  async function GetUser(token) {
+    try {
+      const res = await requestGetUser(token)
+      setData(res)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   async function sessionUser(data) {
     try {
       const res = await requestSessionUser(data)
       localStorage.setItem('@DOGS', res.token)
       setToken(res.token)
+      setLogin(true)
+
+      GetUser(res.token)
     } catch (error) {
+      console.log(error)
       throw new Error('puts')
     }
   }
 
   return (
-    <LoginContext.Provider value={{ token, setToken, sessionUser }}>
+    <LoginContext.Provider value={{ token, setToken, sessionUser, data }}>
       {children}
     </LoginContext.Provider>
   )
--- src/page/Login/LoginForm/LoginForm.jsx
+++ src/page/Login/LoginForm/LoginForm.jsx
@@ -1,27 +1,57 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 
-export const LoginForm = () => {
+import { yupResolver } from '@hookform/resolvers/yup'
 
-  const { register, handleSubmit } = useForm()
+import { validationLoginForm } from '../../../validators/validatorsLoginForm'
+import { LoginFormContainer } from './style'
+import { LoginContext } from '../../../contenxt/LoginProvider/LoginProvider'
+import { Input } from '../../../components/Input/Input'
+import { Button } from '../../../components/Button/Button'
+
+export const LoginForm = () => {
+  const { token, setToken, sessionUser } = useContext(LoginContext)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationLoginForm),
+  })
+
+  function handleSubmitLogin(data) {
+    const { username, password } = data
+    const newLogin = {
+      username,
+      password,
+    }
+    sessionUser(newLogin)
+  }
 
   return (
     <LoginFormContainer>
       <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
+      <form onSubmit={handleSubmit((data) => handleSubmitLogin(data))}>
+        <Input
+          label="Usuário"
           type="text"
-          id='username'
-          {...register('username')}
-          
+          name="username"
+          register={register}
+          errosMessage={errors.username?.message?.toString()}
         />
-        <input
-          type="text"
-          id='password'
-          {...register('password')}
+
+        {/* <input type="text" id="password" {...register('password')} /> */}
+        <Input
+          label="Senha"
+          type="password"
+          name="password"
+          register={register}
+          errosMessage={errors.password?.message?.toString()}
         />
-        <button>Entrar</button>
+
+        <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </LoginFormContainer>
