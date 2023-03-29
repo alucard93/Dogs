75d4059b35d537f34e12c8f5212cdb79ea1a7621
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-03-29 18:17:06 -0300

    feat: User
    estilização User

--- src/components/Photos/PhotoContent/PhotoContent.jsx
+++ src/components/Photos/PhotoContent/PhotoContent.jsx
@@ -4,24 +4,26 @@ import { PhotoComments } from '../PhotoComments/PhotoComments'
 
 import { PhotoContentContainer } from './style'
 
+import view from '../../../assets/visualizacao-black.svg'
+
 export const PhotoContent = ({ dataPhotos }) => {
   const [photo, comments] = dataPhotos
 
   return (
-    <PhotoContentContainer>
+    <PhotoContentContainer view={view}>
       <div className="img">
         <img src={photo.src} alt={photo.title} />
       </div>
       <div className="details">
         <div>
-          <p>
+          <p className='author'>
             <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
             <span className="view">{photo.acessos}</span>
           </p>
           <h1 className="title">
             <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
           </h1>
-          <ul className="attruibutes">
+          <ul className="attributes">
             <li>{photo.peso} kg</li>
             <li>
               {photo.idade === '1'
